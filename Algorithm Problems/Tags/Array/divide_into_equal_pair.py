743b10f939f594db5d2d29682903b7505e10815a
Azizbek Gaybullaev <goazizbek@gmail.com>
2022-04-06 16:14:57 +0500

    new solution added

---
+++ Algorithm Problems/Tags/Array/divide_into_equal_pair.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.discard(num)
+            else:
+                seen.add(num)
+        return not seen
