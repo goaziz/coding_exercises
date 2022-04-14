e97fd7af4956eb191a654eb8ccd114e79f4a3aba
Azizbek Gaybullaev <goazizbek@gmail.com>
2022-04-14 17:40:42 +0500

    new solution added

---
+++ Algorithm Problems/Tags/Array/min_abs_diff.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        abs_val = max(arr)
+        ans = []
+        arr.sort()
+        for i in range(len(arr) - 1):
+            diff = abs(arr[i] - arr[i + 1])
+            abs_val = min(abs_val, diff)
+
+        for i in range(len(arr) - 1):
+            if abs(arr[i] - arr[i + 1]) == abs_val:
+                ans.append([arr[i], arr[i + 1]])
+        return ans
+
+
+obj = Solution()
+arr = [4, 2, 1, 3]
+print(obj.minimumAbsDifference(arr))
