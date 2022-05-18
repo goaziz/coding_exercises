6316ccbeb8eecf61894476f608bdc2fac8b11e5b
Azizbek Gaybullaev <goazizbek@gmail.com>
2022-05-18 21:52:47 +0500

    new solution added

--- Algorithm Problems/Tags/Array/keyboard_row.py
+++ Algorithm Problems/Tags/Array/keyboard_row.py
@@ -6,9 +6,3 @@ class Solution:
         keyboard = [set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')]
         res = [word for word in words if any(set(word.lower()) <= row for row in keyboard)]
         return res
-
-
-obj = Solution()
-words = ["Hello", "Alaska", "Dad", "Peace"]
-
-print(obj.findWords(words))
